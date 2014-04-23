db17c219d2e27098228eea41d659137f6a159875
sithera <sithera11@gmail.coom>
2014-04-23 18:59:13 +0200

    Zamiana tekstu na ciąg bitów

---
+++ string_input.m
@@ -0,0 +1,17 @@
+function [bit_array] = string_input()
+
+% Funkcja zamieniaj¹ca podane przez u¿ytkownika tekst na ci¹g bitów.
+% Odwrotna konwersja w komentarzu, przyda siê póŸniej
+
+word = inputdlg('Podaj tekst: ', 'QAM')         % wywo³anie okna dialogowego 
+
+word_bin = dec2bin(char(word),8)  ;             % zamiana cell na char-bin, ka¿dy znak zapisany jest na 8 bitach
+
+for i = 1:size(word_bin,1)                      % zamiana char-bin na matrix-bin gdzie
+    for j = 1:size(word_bin,2)                  % ka¿da kolumna to 1 bit
+        word_out(1,(i-1)*size(word_bin,2)+j) = str2num(word_bin(i,j));
+    end
+end
+bit_array = word_out;
+end
+%received_array = char(bin2dec(reshape(char('0' + word_out),8,[]).'))' % odwrotna konwersja
