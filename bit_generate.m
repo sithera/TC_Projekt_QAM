bcff667363fd5c1d8ce4699ace06b5ba2587b0e9
sithera <sithera11@gmail.coom>
2014-04-23 18:58:27 +0200

    Generator bitów

---
+++ bit_generate.m
@@ -0,0 +1,13 @@
+function [ random_bits ] = bit_generate(  )
+%Funkcja generuj¹ca ci¹g losowych bitów
+
+M = 16;                     % Rozmiar konstelacji
+k = log2(M);                % Liczba bitów na symbol
+n = 64;                     % Liczba bitów do przes³ania
+
+random_bits = randi([0 1], 1, n);  %wygenerowanie losowych bitow
+
+
+
+end
+
