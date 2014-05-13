8160045c54a9af6b8471a6ecf8245090171f020f
WildThing93 <patrykbogusz93@gmail.com>
2014-05-13 14:33:57 +0200

    Modulujemy
    qam orad demodulacja

---
+++ demqam1.m
@@ -0,0 +1,46 @@
+function [dataWyj] = demqam1(Id,Qd, P)
+dataWyj=[]
+for i=1:1:P
+    
+        if Id(i)==-1 && Qd(i)==-1
+        dataWyj=[dataWyj 0 1 1 1];
+    elseif Id(i)==-1 && Qd(i)==-3
+        dataWyj=[dataWyj 0 1 1 0];
+    elseif Id(i)==-1 && Qd(i)==3
+        dataWyj=[dataWyj 0 1 0 0];
+    elseif Id(i)==-1 && Qd(i)==1
+        dataWyj=[dataWyj 0 1 0 1];
+
+    elseif Id(i)==-3 && Qd(i)==-3
+        dataWyj=[dataWyj 0 0 1 0];
+    elseif Id(i)==-3 && Qd(i)==3
+        dataWyj=[dataWyj 0 0 0 0];
+    elseif Id(i)==-3 && Qd(i)==1
+        dataWyj=[dataWyj 0 0 0 1];
+    elseif Id(i)==-3 && Qd(i)==-1
+        dataWyj=[dataWyj 0 0 1 1];
+
+    elseif Id(i)==1 && Qd(i)==-1
+        dataWyj=[dataWyj 1 1 1 1];
+    elseif Id(i)==1 && Qd(i)==-3
+        dataWyj=[dataWyj 1 1 1 0];
+    elseif Id(i)==1 && Qd(i)==3
+        dataWyj=[dataWyj 1 1 0 0];
+    elseif Id(i)==1 && Qd(i)==1
+        dataWyj=[dataWyj 1 1 0 1];
+
+    elseif Id(i)==3 && Qd(i)==-1
+        dataWyj=[dataWyj 1 0 1 1];
+    elseif Id(i)==3 && Qd(i)==-3
+        dataWyj=[dataWyj 1 0 1 0];
+    elseif Id(i)==3 && Qd(i)==3
+        dataWyj=[dataWyj 1 0 0 0];
+    elseif Id(i)==3 && Qd(i)==1
+        dataWyj=[dataWyj 1 0 0 1];
+  
+    end
+
+end
+
+end
+
