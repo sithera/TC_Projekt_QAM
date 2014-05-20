f41308ecde3a463084db59f30704cc0de934930c
sithera <sithera11@gmail.coom>
2014-05-20 17:54:38 +0200

    Generator bitów

--- bit_generate.m
+++ bit_generate.m
@@ -1,11 +1,13 @@
 function [ random_bits ] = bit_generate(  )
 %Funkcja generuj¹ca ci¹g losowych bitów
 
-M = 16;                     % Rozmiar konstelacji
-k = log2(M);                % Liczba bitów na symbol
-n = 64;                     % Liczba bitów do przes³ania
+n = inputdlg(sprintf('Podaj liczbê paczek bitów do wys³ania:  \n             (Jedna paczka = 4 bity) \n\nUWAGA! Przy podaniu zbyt du¿ej liczby,\nsymulacja mo¿e trwaæ na prawdê d³ugo'), 'QAM');        
+                                        % Liczba bitów do przes³ania
+n = 4*(str2num(cell2mat(n)));
+M = 16;                                 % Rozmiar konstelacji
+k = log2(M);                            % Liczba bitów na symbol                     
 
-random_bits = randi([0 1], 1, n);  %wygenerowanie losowych bitow
+random_bits = randi([0 1], 1, n);       % Wygenerowanie losowych bitow
 
 
 end
