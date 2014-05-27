9c0169c1685eb6f7245624120279f477478a88b6
sithera <sithera11@gmail.coom>
2014-05-27 21:35:58 +0200

    Bit Error Rate

--- BER.m
+++ BER.m
@@ -1,8 +1,14 @@
+for snr=1:1:15
+ber(snr) = qamm(snr)
+end
 k=4;
 Eb_N0_dB  = [0:15];
 theoryBer = (1/k)*3/2*erfc(sqrt(k*0.1*(10.^(Eb_N0_dB/10))));
+figure(10)
 semilogy(Eb_N0_dB,theoryBer,'bs-','LineWidth',2);
-axis([0 15 10^-5 1])
+hold on
+semilogy(Eb_N0_dB,ber,'mx-','LineWidth',2);
+axis([0 15 10^-6 1])
 grid on
 legend('theory', 'simulation');
 xlabel('Eb/No, dB')
