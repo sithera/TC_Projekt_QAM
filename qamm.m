2f89d2e57a1255bc819ed9f5005c813ebcbd34c4
sithera <sithera11@gmail.coom>
2014-06-04 12:20:03 +0200

    Main

--- qamm.m
+++ qamm.m
@@ -1,17 +1,29 @@
-%clear all
-%close all
-function [err_sum, dataWyj] = qamm(snr,data)
+function [err_sum, dataWyj] = qamm(snr,data,wart)
 %data= [0 0 0 0 0 0 0 0 ]
 %data = [  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ]
 %data=[     1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
 %data=[ 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0  0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0  1 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0  0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0  0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 1 1 1 1 0 0 0 1 0  0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0  0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0  0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0  0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0]
 %data=[0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 ]
-%data=[ 0 0 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1]
+%data=[ 0 0 1 1 0 0 0 0 0 0 0 0 1 1 1 1 1 1 ]
 %data=[0 0 0 0 0 1 0 1 1 0 1 0 1 1 1 1]
 %data = bit_generate();
 
-[I,Q]=qam1(data);    %nasza funkcja
-P=(numel(data))/4;
+
+%Wybierz sposób modulacji
+%wart= 4;  % lub 4 lub 16
+if wart == 4
+    W=2;         % W czyli iloœæ bitow w paczce
+    [I,Q]=qam4(data);
+elseif wart == 16
+    W=4;
+    [I,Q]=qam16(data);
+else
+    W=6;
+    [I,Q]=qam64(data);
+end;
+
+%nasza funkcja
+P=(numel(data))/W;
 
 %SYGNAL
 fc=1000;
@@ -33,7 +45,7 @@ t1 = 1:1:length(S);
 setappdata(0,'max_freq',TP);
 
 %AWGN                       t*P
-%snr=5;
+%snr=10;
 %SS = awgn(S,snr,'measured');
 Szum = awgn1( snr, S ,fs,fc);
 SS=Szum+S;  % sygnal plus szum
@@ -46,52 +58,43 @@ for i=1:1:P    %dolno
     Scos((i-1)*100+t+1)=SS((i-1)*100+t+1).*cos(2*pi*f0*t);
     Ssin((i-1)*100+t+1)=SS((i-1)*100+t+1).*sin(2*pi*f0*t);
 end
-%BI = Scos;
-%BQ = Ssin;
-[b,a]=butter(4,0.1);
-BI=2.*filter(b,a,Scos);
-BQ=2.*filter(b,a,Ssin);
+% BI = Scos;
+% BQ = Ssin;
+[b,a]=butter(5,0.2);%(4,0.15);
+BI=2*filtfilt(b,a,Scos);%2.*filter(b,a,Scos);
+BQ=2*filtfilt(b,a,Ssin);%2.*filter(b,a,Ssin);
 %subplot(3,1,2); plot(Scos) title('syg*cos')
 %subplot(3,1,3); plot(BI) title('po filtrze')
 
 IdSr=[ ];                      %DEModulacja
 QdSr=[ ];
 for i=1:1:P    %dolno
-    IdSr(i)= mean(BI(i+(((i-1)*100)):(i*100)));
-    QdSr(i)= mean(BQ(i+(((i-1)*100)):(i*100)));
+    IdSr(i)= mean(BI((1+(i-1)*100):(i*100)));
+    QdSr(i)= mean(BQ((1+(i-1)*100):(i*100)));
 end
 Id=[];
 Qd=[];
-for i=1:1:P
-    if IdSr(i)>=2
-        Id(i)=3;
-    elseif IdSr(i)<2 && IdSr(i)>=0
-        Id(i)=1;
-    elseif IdSr(i)>= -2 && IdSr(i)<0
-        Id(i)=-1;
-    else IdSr(i)< -2
-        Id(i)=-3;
-    end
-    
-    if QdSr(i)>=2
-        Qd(i)=-3;
-    elseif QdSr(i)<2 && QdSr(i)>=0
-        Qd(i)=-1;
-    elseif QdSr(i)>= -2 && QdSr(i)<0
-        Qd(i)=1;
-    else QdSr(i)< -2
-        Qd(i)=3;
-    end
-    
+
+setappdata(0,'IdSr',IdSr);
+setappdata(0,'QdSr',QdSr);
+
+%demodulacja
+if wart == 4
+    [ Id, Qd ] = demqam4( QdSr, IdSr , P);
+    [dataWyj]=dem2qam4(Id,Qd,P);
+elseif wart ==16
+    [ Id, Qd ] = demqam16( QdSr, IdSr , P);
+    [dataWyj]=dem2qam16(Id,Qd,P);
+else
+    [ Id, Qd ] = demqam64( QdSr, IdSr , P);
+    [dataWyj]=dem2qam64(Id,Qd,P);
 end;
 
-[dataWyj]=demqam1(Id,Qd,P);
 
-t2=1:100;
 Sodt=[];
 for i=1:1:P        % odtworzony sygnal
-    sd1=Id(i)*cos(2*pi*f0*t2);    %  fc/fs  ??
-    sd2=Qd(i)*sin(2*pi*f0*t2);
+    sd1=Id(i)*cos(2*pi*f0*t);    %  fc/fs  ??
+    sd2=Qd(i)*sin(2*pi*f0*t);
     Sodt=[Sodt (sd1-sd2)];    %czysty
 end
 
@@ -99,33 +102,33 @@ err_sum = sum(abs(data - dataWyj));
 setappdata(0,'S',S);
 setappdata(0,'Sodt',Sodt);
 
-%figure(1)
-%axis([0 TP -5 5 ])
-%subplot(3,1,1);
-%plot(S)
-%title('16-QAM Modulated Signal')
-%axis([0 TP -5 5 ])   %skalaa
-%subplot(3,1,2);
-%plot(SS)   %
-%title('16-QAM Modulated Signal+Noise')
-%axis([0 TP -5 5 ])   %skalaa
-%subplot(3,1,3);
-%plot(Sodt)   %
-%title('Sygnal odtworzony')
-%axis([0 TP -5 5 ])   %skalaa
-
-figure(2)
-subplot(4,1,1);
-plot(Ssin)
-title('syg*sin')
-subplot(4,1,2);
-plot(BQ)
-title('sin po filtrze')
-
-subplot(4,1,3);
-plot(Scos)
-title('syg*cos')
-subplot(4,1,4);
-plot(BI)
-title('cos po filtrze')
-end
+% figure(1)
+% axis([0 TP -7 7 ])
+% subplot(3,1,1);
+% plot(S)
+% title('16-QAM Modulated Signal')
+% axis([0 TP -10 10 ])   %skalaa
+% subplot(3,1,2);
+% plot(SS)   %
+% title('16-QAM Modulated Signal+Noise')
+% axis([0 TP -10 10 ])   %skalaa
+% subplot(3,1,3);
+% plot(Sodt)   %
+% title('Sygnal odtworzony')
+% axis([0 TP -10 10 ])   %skalaa
+%
+% figure(2)
+% subplot(4,1,1);
+% plot(Ssin)
+% title('syg*sin')
+% subplot(4,1,2);
+% plot(BQ)
+% title('sin po filtrze')
+%
+% subplot(4,1,3);
+% plot(Scos)
+% title('syg*cos')
+% subplot(4,1,4);
+% plot(BI)
+% title('cos po filtrze')
+%end
