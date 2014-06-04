357216a37c930d3d0de1b8e9d1901cfd1b0b09db
sithera <sithera11@gmail.coom>
2014-06-04 12:19:20 +0200

    BER

--- BER.m
+++ BER.m
@@ -1,16 +1,25 @@
-for snr=1:1:15
-ber(snr) = qamm(snr)
-end
-k=4;
-Eb_N0_dB  = [0:15];
-theoryBer = (1/k)*3/2*erfc(sqrt(k*0.1*(10.^(Eb_N0_dB/10))));
-figure(10)
-semilogy(Eb_N0_dB,theoryBer,'bs-','LineWidth',2);
+function [] = BER();
+
+%-------------- Porównanie QAM4 vs QAM16 vs QAM64 --------------%
+
+wykres = getappdata(0,'wykres');
+axes(wykres);
+EbNodB=1:25;
+EbNo=10.^(EbNodB/10);
+
+semilogy(EbNodB,(4/6)*(1-1/sqrt(64))*(1/2)*erfc(sqrt(3*6*EbNo/(64-1))/sqrt(2)),'rs-','LineWidth',2);
 hold on
-semilogy(Eb_N0_dB,ber,'mx-','LineWidth',2);
-axis([0 15 10^-6 1])
+semilogy(EbNodB,(4/4)*(1-1/sqrt(16))*(1/2)*erfc(sqrt(3*4*EbNo/(16-1))/sqrt(2)),'gs-','LineWidth',2);
+hold on
+semilogy(EbNodB,(4/2)*(1-1/sqrt(4))*(1/2)*erfc(sqrt(3*2*EbNo/(4-1))/sqrt(2)),'bs-','LineWidth',2);
+axis([0 25 10^-6 1])
 grid on
-legend('theory', 'simulation');
+legend('QAM 64', 'QAM 16','QAM 4');
 xlabel('Eb/No, dB')
 ylabel('Bit Error Rate')
 title('Bit error probability curve for 16-QAM modulation')
+hold off
+
+%---------------------------------------------------------------%
+
+end
